68ff275b9389cada3062565bffdf67efdb8f487e
chloe-tt <chloe.trottin.chine@gmail.com>
2020-04-23 20:28:12 +0200

    add play cta route

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+ 
+
+import SelectPlayer from './component/SelectPlayerPage/SelectPlayer';
+
+
+import LandingPage from './component/LandingPage/LandingPage';
+
+
+ 
+class App extends Component {
+  render() {
+    return (      
+       <BrowserRouter>
+        <div>
+        
+            <Switch>
+             <Route path="/" component={LandingPage} exact/>
+             <Route path="/select-player" component={SelectPlayer}/>
+       
+           </Switch>
+        </div> 
+      </BrowserRouter>
+    );
+  }
+}
+
+
+export default App;
---
+++ src/component/Intro/Intro.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Intro.css';
+import ReactPlayer from "react-player";
+import { NavLink } from 'react-router-dom';
+
+
+function Intro() {
+  return (
+    <div className="general">
+        <div className="bg">
+          <div className="intro-ring">
+         
+          <iframe src='https://www.youtube.com/embed/19qYDTNbGwA?autoplay=1'
+            frameBorder='0'
+            allow='autoplay; encrypted-media'
+            allowFullScreen
+            title='video'
+            width={535}
+            height={350}
+            className="movie" 
+          />
+           </div>
+ 
+        </div> 
+       <div className="containerBouton">
+        <button className="selectPlayerContainer2">
+           <NavLink className="toSelectPlayer" to="/select-player">Tenter l'aventure !</NavLink>        
+           </button>
+           </div>
+
+    </div>
+  );
+}
+
+export default Intro;
