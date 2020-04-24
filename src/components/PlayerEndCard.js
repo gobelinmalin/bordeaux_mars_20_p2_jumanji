16d18c7893bae9f188d8b1b42ab8b0e9ba45e698
bz699 <zeebha@gmail.com>
2020-04-24 04:13:07 +0200

    EndScreen justify version, animated sphere, animated cards

---
+++ src/components/PlayerEndCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './PlayerEndCard.css';
+
+function PlayerEndCard () {
+    return (
+        <div className="EndCard">
+            <div className="CharacterPic"></div>
+            <div className="CharacterName"></div>
+        </div>
+    )
+}
+
+export default PlayerEndCard;
