ad6b480e54fbbb6dd53b31d1a1028517519d9653
chloe-tt <61197607+chloe-tt@users.noreply.github.com>
2020-05-03 19:37:18 +0200

    conflict resolution to merge with feat/plateau

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+ 
+
 import SelectPlayer from './component/SelectPlayerPage/SelectPlayer';
-import LandingPage from './component/LandingPage/LandingPage'
+
+
+import LandingPage from './component/LandingPage/LandingPage';
+import Plateau from './component/Plateau/Plateau'
+
 
 
  
@@ -14,11 +20,14 @@ class App extends Component {
             <Switch>
              <Route path="/" component={LandingPage} exact/>
              <Route path="/select-player" component={SelectPlayer}/>
+             <Route path="/plateau" component={Plateau}/>
+       
            </Switch>
         </div> 
       </BrowserRouter>
     );
   }
 }
+
 
 export default App;
