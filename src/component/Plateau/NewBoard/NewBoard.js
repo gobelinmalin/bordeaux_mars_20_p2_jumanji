8af6d9fd4d5f15c2b17a037483ca83203bba638d
rachOS <g.chamekh@e-nautia.com>
2020-05-06 01:55:05 +0200

    cleaning code

--- src/component/Plateau/NewBoard/NewBoard.js
+++ src/component/Plateau/NewBoard/NewBoard.js
@@ -16,138 +16,81 @@ class NewBoard extends React.Component {
         }
     }
 
-    // Création du plateau de cases
-    /* createBoard = () => {
-        let tileArray = []
-        for (let counter = 100; counter > 0; counter--) {
-            tileArray.push(counter)
+    // Gestion du tour de jeu
+    // currentPlayer < nbr de joueurs engagés
+    handlePlayerTurn = () => {
+        let currentPlayer = this.state.playerTurn;
+        if (currentPlayer < 2) {
+            this.setState({ playerTurn: currentPlayer + 1 })
+        } else {
+            this.setState({ playerTurn: 1 })
         }
-        const board = tileArray.map((tile, tileIndex) => <div key={tileIndex} className={this.tileColor ? "TilePathPlayer1" : "TileNeutral"}>{tileIndex}</div>)
-
-        for (let i = 0; i < board.length; i++) {
-
-            //console.log(board[i].key, "board")
-        }
-        return board  // <== !!!! this.state.board
-    } */
-
+        console.log(currentPlayer)
+    }
 
     componentWillMount() {
         this.setState({ board: [this.createBoard()] })
         console.log("WILLMOUNT")
     }
-    componentDidUpdate() {
-        return this.handleChangeColor
+
+    componentWillUpdate() {
         console.log("WILLUPDATE")
     }
 
-    /* componentDidMount(pP, pS) {
-
-        this.setState({ board: [this.createBoard()], color: true })
-        console.log(pS, pP, "DIDUPDATE")
-    } */
-
     createBoard = () => {
         let tileArray = []
+        let board = this.state.board
         const pathPlayer = this.state.path[0].pathPlayer1.path
 
         for (let counter = 100; counter > 0; counter--) {
             tileArray.push(counter)
         }
 
-        const board = tileArray.map((tile, tileIndex) =>
-            tileIndex === 0 || tileIndex === 1
-                ?
-                <div
-                    key={tileIndex}
-                    className={
+        for (let i = 0; i < pathPlayer.length; i++) {
+            board = tileArray.map((tile, tileIndex) =>
 
-                        "TilePathPlayer1"
+                pathPlayer.filter(path => tileIndex === path).length !== 0
+                    ?
+                    <div
+                        key={tileIndex}
+                        className={
 
-                    }>
-                    {tileIndex}
-                </div>
-                :
-                <div
-                    key={tileIndex}
-                    className={
+                            "TilePathPlayer1"
 
-                        "TileNeutral"
+                        }>
+                        {tileIndex}
+                    </div>
+                    :
+                    <div
+                        key={tileIndex}
+                        className={
 
-                    }>
-                    {tileIndex}
-                </div>)
+                            "TileNeutral"
+
+                        }>
+                        {console.log("YEAH")}
+                        {tileIndex}
+                    </div>)
+        }
 
         return board
     }
 
     handleChangeColor = () => {
         this.setState({ color: !this.state.color })
-        // console.log(this.state.color, "KOLOR")
-
     }
-
-
-    // Gestion du tour de jeu
-    //  handlePlayerTurn(){
-    //    let currentPlayer = this.state.playerTurn;
-    //    this.setState({playerTurn: currentPlayer++})
-    // }
-
-
-    //On compare board avec pathPlayers / si cell PathPlayers = cell board alors renvoie true pour la cell
-
-    isThisTileInPathPlayer(array, pathArray) {
-        array.some(number => pathArray.indexOf(number) !== -1)
-
-    }
-
-
-    /* let updatedTile = this.state.til
-
-
-
-    /* this.isThisTileInPathPlayer(createBoard(), pathArray) {
-
-        const board = this.createBoard().map((i, j) => j)
-        const pathCurrentPlayer = this.state.path[0].pathPlayer1.path
-
-        createBoard().some(number => console.log(pathCurrentPlayer.indexOf(number) !== -1));
-    } */
-
-    //number => number.map(pathArray.indexOf(number) !== -1)
-
-    //Changer la couleur de la tuile
-    /* changeTileColor() {
-        if (isThisTileInPathPlayer) {
-            setTileColor(true);
-        } else {
-            setTileColor(false);
-        }
-    } */
-
-
-    /* displayPath = () => {
-        const pathCurrentPlayer = this.state.path[0].pathPlayer1.path
-        const displayPath = pathCurrentPlayer.map(i => i)
-        const board = this.createBoard().map((i, j) => j)
-        const test = board.some(i => i === displayPath && <div> test</div>)
-        console.log(test)
-    } */
 
     render() {
         const pathCurrentPlayer = this.state.path
         console.log(this.state.color, "STATE")
         return (
             <div className="game-board">
-                {/* <Tile tileColor={this.state.tileColor}/> */}
                 <Grid
                     width={50}
                     gap={5}
                 >
                     {this.state.board[0]}
-                    <button onClick={this.handleChangeColor}> {this.state.color}</button>
-                    {/* {this.createBoard()} */}
+                    <button onClick={this.handlePlayerTurn}>ChangeTurn</button>
                 </Grid>
             </div>
         )
--- src/component/Plateau/NewBoard/pathPlayers.js
+++ src/component/Plateau/NewBoard/pathPlayers.js
@@ -1,10 +1,17 @@
 const pathsPlayers = [
     {
         pathPlayer1: {
-            idP1:1,
+            Player:1,
             startTile: 0,
-            path: [1,2,3,4,5,6,7,8,18,28,38,48,47],
-            color: "cyan"
+            path: [0,1,2,3,4,5,6,7,8,18,28,38,48,47],
+            color: true
+        },
+
+        pathPlayer2: {
+            Player:2,
+            startTile: 0,
+            path: [9,19,29,39,49,59,69,79,78,77,76,75,65],
+            color: "blue"
         },
 
     }
